a8fe0203bd253f8ec0b967b2466ac5999db0cd0b
JulianaNishimura <juliana.nishimura.tds24@gmail.com>
2025-02-21 21:11:08 -0300

    pastas

---
+++ src/main/java/com/example/Atividade_ExtraSala_BackEnd/AtividadeExtraSalaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.Atividade_ExtraSala_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeExtraSalaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeExtraSalaBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Atividade_ExtraSala_BackEnd/AtividadeExtraSalaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Atividade_ExtraSala_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeExtraSalaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
